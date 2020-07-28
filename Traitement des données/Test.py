0e2e3a061f261405b2f06a58eca0bb4ba835762e
Amaury Semery <amaury.semery@laposte.net>
2020-07-28 10:45:53 +0200

    Update Test.py

--- Traitement des données/Test.py
+++ Traitement des données/Test.py
@@ -1,2 +1,5 @@
 import csv
 with open('/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers CSV/classement_SSAoG_CSV.csv', newline'') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
