22222b8a40a45751d976708c2973ab9ed9fb1a63
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-28 19:42:16 +0200

    Update Test 7.py

--- Traitement des données/Test 7.py
+++ Traitement des données/Test 7.py
@@ -12,47 +12,22 @@ Votes = []
 
 for ligne in filename:
     a = ligne[0]
-    b = ligne[1]
+    b = int(ligne[1])
     c = int(ligne[2])
     Name.append(a)
     Classement.append(b)
     Votes.append(c)
 
 print(Name)
-print(Classement)
 print(Votes)
+print(Classement)
+print(len(Votes)) # Compte le nombre d'items de la liste
 
 
 def convertisseur(Votes):
     XP = []
-    for c in range(c+1):
+    for c in range(len(Votes)):
         convertisseur = lambda c: c==int(3) if c >= 50 and c < 100 else(c==int(5) if c > 100 else c == int(0))
         print(XP)
 
 print(convertisseur(Votes))
-
-
-def conversion(c):
-    if c < 50:
-        return 0
-    elif c >= 50 and c < 100:
-        return 3
-    else:
-        return 5
-
-print(map(conversion, Votes))
-
-def liste_finale(filename):
-    XP = []
-    f = open('C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\classement_SSAoG_CSV_V2.csv', 'r')
-    for line in f:
-        liste=[]
-        Name, Classement, Votes = line.split(',')
-        data = {}
-        data["Name"]=Name.strip()
-        data["Classement"]=Classement.strip()
-        data["Votes"]=Votes.strip()
-        liste.append(data)
-    return liste
-
-# print(liste_finale(filename))
