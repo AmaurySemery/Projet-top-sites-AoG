d2c6f6a78d5c97506ced2d4f6535aa53125db941
Amaury Semery <amaury.semery@laposte.net>
2020-07-28 12:48:43 +0200

    0

--- Traitement des données/Test.py
+++ Traitement des données/Test.py
@@ -3,6 +3,7 @@
 #... et affichage des lignes.
 import csv
 with open('/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/classement_SSAoG_CSV_V2.csv',newline='') as f:
-    reader = csv.reader(f)
+    tableau=[]
+    lire= csv.reader(f)
     for row in reader:
         print(row)
