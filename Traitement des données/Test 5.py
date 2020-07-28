895fd6c914c2d01c5cf9f5e5e80ed9b0a21f0667
Amaury Semery <amaury.semery@laposte.net>
2020-07-28 16:17:35 +0200

    Update Test 5.py

--- Traitement des données/Test 5.py
+++ Traitement des données/Test 5.py
@@ -25,40 +25,12 @@ for i in range(n-1):
 
 print(Name, Classement, Votes)
 
-import pandas as pd
+def XP(i):
+    if i < 50:
+        i = 0
+    if i >= 50 and i < 100:
+        i = 3
+    if i > 100:
+        i = 5
 
-# the file names
-f1 = Name
-f2 = Classement
-f3 = Votes
-out_f = "/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/tableau_test.csv"
-
-# read the files
-df1 = pd.read_csv(f1)
-df2 = pd.read_csv(f2)
-df3 = pd.read_csv(f3)
-
-# get the keys
-keys1 = list(df1)
-keys2 = list(df2)
-keys3 = list(df3)
-
-# merge both files
-for idx, row in df2.iterrows():
-    data = df1[df1['id'] == row['id']]
-
-    # if row with such id does not exist, add the whole row
-    if data.empty:
-        next_idx = len(df1)
-        for key in keys2:
-            df1.at[next_idx, key] = df2.at[idx, key]
-
-    # if row with such id exists, add only the missing keys with their values
-    else:
-        i = int(data.index[0])
-        for key in keys2:
-            if key not in keys1:
-                df1.at[i, key] = df2.at[idx, key]
-
-# save the merged files
-df1.to_csv(out_f, index=False, encoding='utf-8', quotechar="", quoting=csv.QUOTE_NONE)
+print map(XP, Votes)
