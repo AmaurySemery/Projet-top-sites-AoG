3574ae68f7eb4b969a15741dba658d7d3d39418d
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-28 18:33:47 +0200

    0

--- Traitement des données/Test 7.py
+++ Traitement des données/Test 7.py
@@ -5,8 +5,22 @@ import smopy
 
 f = open('C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\classement_SSAoG_CSV_V2.csv', 'r')
 filename =  csv.reader(f)
+
+Name = []
+Classement = []
+Votes = []
+
 for ligne in filename:
-    print(ligne)
+    a = ligne[0]
+    b = ligne[1]
+    c = float(ligne[2])
+    Name.append(a)
+    Classement.append(b)
+    Votes.append(c)
+
+print(Name)
+print(Classement)
+print(Votes)
 
 def liste_finale(filename):
     XP = []
