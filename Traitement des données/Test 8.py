b56df05d7eab2fd98d68b0cd0fbfce92fc95c9a8
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-28 21:23:54 +0200

    Update Test 8.py

--- Traitement des données/Test 8.py
+++ Traitement des données/Test 8.py
@@ -1,7 +1,4 @@
 import csv
-import requests
-import config
-import smopy
 
 f = open('C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\classement_SSAoG_CSV_V2.csv', 'r')
 filename =  csv.reader(f)
