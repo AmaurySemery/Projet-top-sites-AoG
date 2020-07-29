b7f16cdd17cf3ed3e5936d4657170762867fea26
Amaury Semery <amaury.semery@laposte.net>
2020-07-29 11:57:49 +0200

    Create Test 9.py

---
+++ Traitement des données/Test 9.py
@@ -0,0 +1,27 @@
+import csv
+
+f = open('/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/classement_SSAoG_CSV_V2.csv', 'r')
+filename =  csv.reader(f)
+
+
+def conversion(number):
+    if number < 50:
+        result = 0
+    if number >= 50 and number < 100:
+        result = 3
+    if number > 100:
+        result = 5
+    return result
+    #    print(+ int(result))
+
+Name = []
+Votes = []
+XP = []
+
+for l in filename:
+    a = l[0]
+    b = int(l[1])
+    Name.append(a)
+    Votes.append(b)
+    XP.append(conversion(b))
+
