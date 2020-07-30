f44422a2e05b5d79792fe97931160ce9e4c514cd
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-30 18:09:49 +0200

    Update Final prototype.py

--- Traitement des données/Final prototype.py
+++ Traitement des données/Final prototype.py
@@ -3,26 +3,26 @@ import csv
 # Lire le fichier, puis garder les colonnes qui m'intéressent, le nouveau tableau étant envoyé dans un autre fichier
 
 import pandas as pd
-f=pd.read_csv("/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/upgrade_classement_SSAoG_CSV.csv",sep =';')
+f=pd.read_csv(r"C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\upgrade_classement_SSAoG_CSV.csv",sep =';')
 #print(f)
 keep_col = ['Pseudo','Total']
 #print(keep_col)
 new_f = f[keep_col]
 #print(new_f)
-new_f.to_csv("/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/upgrade1_classement_SSAoG_CSV.csv", index=False)
+new_f.to_csv(r"C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\upgrade1_classement_SSAoG_CSV.csv", index=False)
 #print(keep_col)
 
 # Je récupère le fichier nouvellement créé pour travailler dessus. Je retravaille ensuite le fichier pour supprimer la première ligne, autrement, la chaîne de caractère en première ligne va m'empêcher de faire mes calculs de conversions en int
 
-with open("/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/upgrade1_classement_SSAoG_CSV.csv",'r') as f:
-    with open("/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/upgrade2_classement_SSAoG_CSV.csv",'w') as f1:
+with open(r"C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\upgrade1_classement_SSAoG_CSV.csv",'r') as f:
+    with open(r"C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\upgrade2_classement_SSAoG_CSV.csv",'w') as f1:
         next(f) # skip header line
         for line in f:
             f1.write(line)
 
 # Je récupère le dernier fichier pour retravailler dessus.
 
-f = open('/home/popschool/Documents/GitHub/Projet-top-sites-AoG/Dossiers-CSV/upgrade2_classement_SSAoG_CSV.csv', 'r')
+f = open(r'C:\Divers\Python codes\Projet-top-sites-AoG\Dossiers-CSV\upgrade2_classement_SSAoG_CSV.csv', 'r')
 filename =  csv.reader(f)
 
 
@@ -50,7 +50,11 @@ for l in filename:
     XP.append(conversion(b))
 
 # print(Name,Votes,XP)
-print("<blockquote>[center][b]Récompenses du 22/06 au 28/06[/b][/center]")
+
+datedebut = input("Date début de requête (ex: 22/06): ")
+datefin = input("Date fin de requête (ex: 28/06): ")
+
+print("<blockquote>[center][b]Récompenses du "+datedebut+" au "+datefin+"[/b][/center]")
 print("Pour rappel, vous gagnez 3 XP par semaine si vous effectuez au moins <b>50</b> votes sur celle-ci.")
 print("Vous gagnez 5 XP au total si vous êtes parmi les 5 meilleurs votants ou que vous avez réalisé plus de 100 votes.")
 print("Les récompensés sont donc les suivants :[list=1]")
