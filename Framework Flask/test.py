d186e8366c54f18b8ee89d7d8afae4e598c0c81d
Amaury Semery <amaury.semery@laposte.net>
2020-08-19 15:18:02 +0200

    Update test.py

--- Framework Flask/test.py
+++ Framework Flask/test.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-	return "Coucou!"
+	return "Coucou! <a href='/accueil'> entrez </a>"
+	
+@app.route('/accueil')
+def accueil():
+	return "Bienvenue! <a href='/'> retour </a>"
+	
+@app.route('/pers/<name>/<age>')
+def pers(name,age):
+	return "hellow " + name + age
 
 if __name__ == '__main__':
 	app.run()
