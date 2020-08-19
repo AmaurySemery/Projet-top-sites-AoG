f12bb5755515ac3f5882c547edabbf9c879bd993
Amaury Semery <amaury.semery@laposte.net>
2020-08-19 16:18:04 +0200

    1

--- Framework Flask/test.py
+++ Framework Flask/test.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Coucou! <a href='/accueil'> entrez </a>"
+	return "Coucou! <a href='/accueil'> Accès à l'accueil </a>" + " " + "Hellow <a href='/pers/<name>'> Onglet pers </a>"
 	
 @app.route('/accueil')
 def accueil():
-	return "Bienvenue! <a href='/'> retour </a>"
+	return "Bienvenue! <a href='/'> retour </a>" + " " + "Hellow <a href='/pers/<name>'> Onglet pers </a>"
 	
 @app.route('/pers/<name>')
 def pers(name):
-	return render_template("page1.html",title="Cool !",username=name)
+	return render_template("page1.html",title="Cool !",username=name) + "Hellow <a href='/accueil'> retour </a>"
 
 if __name__ == '__main__':
 	app.run()
